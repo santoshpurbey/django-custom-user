c63d734b0eeeaa3ccc384d524cfb707b5d306606
Santosh Purbey <santosh.purbey@logpoint.com>
2019-02-19 08:10:13 +0545

    start app custom_user and create custom manager in managers.py

--- wow_project/settings.py
+++ wow_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom apps 
+    'apps.custom_user',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'custom_user.CustomUser'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
